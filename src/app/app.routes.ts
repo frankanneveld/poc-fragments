5e7a7c400096ad356b0c26da4f8724d890d4f493
Frank Anneveld <frank@anneveld.it>
2024-02-05 17:18:32 +0100

    boyscout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ContentComponent } from './content/content.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full'},
-    { path: 'home', component: HomeComponent}
+    { path: 'home', component: HomeComponent},
+    { path: 'content', component: ContentComponent},
 ];
